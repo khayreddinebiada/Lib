8b4fbacf1e9669107d921a988b1e31d8cbb535d6
Khayreddine Biada <khayreddine.biada@gmail.com>
2020-12-20 14:09:30 +0300

    Add loading page

---
+++ Assets/Scripts/Lib/LoadingPageManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace game.loading
+{
+    public class LoadingPageManager : MonoBehaviour
+    {
+        [SerializeField]
+        private Text _textLoading;
+        [SerializeField]
+        private Slider _loadingSlider;
+
+        private int _pointNumber;
+        // Start is called before the first frame update
+        private void Start()
+        {
+            _textLoading.text = "LOADING .";
+            StartCoroutine(WaitAndChangeText());
+        }
+
+        private IEnumerator WaitAndChangeText()
+        {
+            yield return new WaitForSeconds(0.8f);
+            _pointNumber++;
+
+            if (_pointNumber == 3)
+            {
+                _textLoading.text = "LOADING ";
+                _pointNumber = 0;
+            }
+
+            _textLoading.text += ".";
+
+            StartCoroutine(WaitAndChangeText());
+            StartCoroutine(LoadAsynchronously(1));
+        }
+
+        IEnumerator LoadAsynchronously(int scenceIndex)
+        {
+            AsyncOperation operation = SceneManager.LoadSceneAsync(scenceIndex);
+
+            while (!operation.isDone)
+            {
+
+                _loadingSlider.value = operation.progress;
+                yield return null;
+
+            }
+
+        }
+    }
+}
