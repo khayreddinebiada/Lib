a217a2ed805437b55e95ada7ce9829f21865f0fa
Khayreddine Biada <khayreddine.biada@gmail.com>
2020-12-21 10:17:33 +0300

    *

---
+++ Assets/Scripts/Lib/GameSceneManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace game.lib
+{
+    public class GameSceneManager : MonoBehaviour
+    {
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+
+        }
+
+        public void Replay()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        public void ReplayAsync()
+        {
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+        }
+
+        public void GoScene(int sceneIndex)
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
+
+        public void GoScene(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+
+        public void GoSceneAsync(int sceneIndex)
+        {
+            SceneManager.LoadSceneAsync(sceneIndex);
+        }
+
+        public void GoSceneAsync(string sceneName)
+        {
+            SceneManager.LoadSceneAsync(sceneName);
+        }
+    }
+}
---
+++ Assets/Scripts/Lib/TraceTarget.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class TraceAxis
+{
+    public bool x = true, y = true, z = true;
+}
+
+public class TraceTarget : MonoBehaviour
+{
+    [SerializeField]
+    private bool _isTrace = false;
+
+    [SerializeField]
+    private Transform _target;
+    private Vector3 _initDeltaPosition;
+    
+
+    [SerializeField]
+    private TraceAxis _traceOn;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _initDeltaPosition = transform.position - _target.position;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (_isTrace)
+        {
+            Vector3 vec = _initDeltaPosition + _target.position;
+            transform.position = new Vector3(
+                (_traceOn.x) ? vec.x : transform.position.x,
+                (_traceOn.y) ? vec.y : transform.position.y,
+                (_traceOn.z) ? vec.z : transform.position.z
+                );
+
+        }
+    }
+}
