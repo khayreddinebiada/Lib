21cab0e0fabb54d336f32bfaf61d893f7d540244
Khayreddine Biada <khayreddine.biada@gmail.com>
2020-12-21 15:21:27 +0300

    Add controller hand

---
+++ Assets/Scripts/Lib/ControllerHand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+
+namespace game.ui
+{
+    public class ControllerHand : MonoBehaviour
+    {
+        #region variables
+        private ControllerInputs _controllerInputs;
+
+        [SerializeField]
+        private Transform backgroundHand;
+        [SerializeField]
+        private Transform movingHand;
+        #endregion
+
+        #region default functions
+        void Start()
+        {
+            _controllerInputs = ControllerInputs.instance;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            MovingHand();
+            ActivateHand();
+        }
+        #endregion
+
+        #region controller hand
+        private void ActivateHand()
+        {
+            if (_controllerInputs.isOnDragging)
+            {
+                backgroundHand.gameObject.SetActive(true);
+            }
+            else
+            {
+                backgroundHand.gameObject.SetActive(false);
+            }
+        }
+
+        private void MovingHand()
+        {
+            Vector3 dragging = _controllerInputs.deltaDragging;
+            movingHand.position =backgroundHand.position + dragging;
+        }
+        #endregion
+    }
+}
